091088932e1f669a803f8f02209f2fe6eabd3cf3
Revalariansah <ariansyahreval5@gmail.com>
2024-03-01 14:29:44 +0700

    update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>@yield('title')</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+ 
+<body>
+    <header class="px-4 py-2 shadow">
+        <div class="flex justify-between">
+            <div class="flex items-center">
+ 
+                <button data-search class="p-4 md:hidden focus:outline-none" type="button">
+                    <svg data-search-icon class="fill-current w-4" viewBox="0 0 512 512" style="top: 0.7rem; left: 1rem;">
+                        <path d="M225.474 0C101.151 0 0 101.151 0 225.474c0 124.33 101.151 225.474 225.474 225.474 124.33 0 225.474-101.144 225.474-225.474C450.948 101.151 349.804 0 225.474 0zm0 409.323c-101.373 0-183.848-82.475-183.848-183.848S124.101 41.626 225.474 41.626s183.848 82.475 183.848 183.848-82.475 183.849-183.848 183.849z" />
+                        <path d="M505.902 476.472L386.574 357.144c-8.131-8.131-21.299-8.131-29.43 0-8.131 8.124-8.131 21.306 0 29.43l119.328 119.328A20.74 20.74 0 00491.187 512a20.754 20.754 0 0014.715-6.098c8.131-8.124 8.131-21.306 0-29.43z" />
+                    </svg>
+                </button>
+ 
+                
+            </div>
+ 
+            <div class="flex items-center">
+                <button data-dropdown class="flex items-center px-3 py-2 focus:outline-none hover:bg-gray-200 hover:rounded-md" type="button" x-data="{ open: false }" @click="open = true" :class="{ 'bg-gray-200 rounded-md': open }">
+                    <img src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&h=100&q=80" alt="Profle" class="h-8 w-8 rounded-full">
+                    <svg class="fill-current w-3 ml-4" viewBox="0 0 407.437 407.437">
+                        <path d="M386.258 91.567l-182.54 181.945L21.179 91.567 0 112.815 203.718 315.87l203.719-203.055z" />
+                    </svg>
+ 
+                    <div data-dropdown-items class="text-sm text-left absolute top-0 right-0 mt-16 mr-4 bg-white rounded border border-gray-400 shadow" x-show="open" @click.away="open = false">
+                        <ul>
+                            <li class="px-4 py-3 border-b hover:bg-gray-200"><a href="#">My Profile</a></li>
+                            <li class="px-4 py-3 hover:bg-gray-200"><a href="{{ route('logout') }}">Log out</a></li>
+                        </ul>
+                    </div>
+                </button>
+ 
+            </div>
+        </div>
+    </header>
+ 
+    <div class="flex flex-row">
+        <div class="flex flex-col w-64 h-screen overflow-y-auto bg-gray-900 border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
+            <div class="sidebar text-center bg-gray-900">
+                <div class="text-gray-100 text-xl">
+                    <div class="p-2.5 mt-1 flex items-center">
+                        <h1 class="font-bold text-gray-200 text-[15px] ml-3">Admin</h1>
+                    </div>
+                    <div class="my-2 bg-gray-600 h-[1px]"></div>
+                </div>
+                <a href="{{ route('admin/home') }}">
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-house-door"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
+                    </div>
+                </a>
+                <a href="{{ route('admin/products') }}">
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-book"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Product</span>
+                    </div>
+                </a>
+                <a href="{{ route('admin/profile') }}">
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                    <i class="bi bi-person"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Profile</span>
+                    </div>
+                </a>
+                <a href="{{ route('logout') }}">
+                    <div class="my-4 bg-gray-600 h-[1px]"></div>
+                    <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="flex flex-col w-full h-screen px-4 py-8 mt-10">
+            <div>@yield('contents')</div>
+        </div>
+    </div>
+</body>
+ 
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,30 +16,40 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+ 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+ 
+Route::get('/about', [UserController::class, 'about'])->name('about');
+ 
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+ 
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+ 
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+ 
+//Normal Users Routes List
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [UserController::class, 'userprofile'])->name('profile');
 });
 
-Route::get('/login', function () {
-    return view('login');
+//Admin Routes List
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin/home');
+ 
+    Route::get('/admin/profile', [AdminController::class, 'profilepage'])->name('admin/profile');
+ 
+    Route::get('/admin/products', [ProductController::class, 'index'])->name('admin/products');
+    Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin/products/create');
+    Route::post('/admin/products/store', [ProductController::class, 'store'])->name('admin/products/store');
+    Route::get('/admin/products/show/{id}', [ProductController::class, 'show'])->name('admin/products/show');
+    Route::get('/admin/products/edit/{id}', [ProductController::class, 'edit'])->name('admin/products/edit');
+    Route::put('/admin/products/edit/{id}', [ProductController::class, 'update'])->name('admin/products/update');
+    Route::delete('/admin/products/destroy/{id}', [ProductController::class, 'destroy'])->name('admin/products/destroy');
 });
-
-Route::get('/login/dashboard', function () {
-    return view('index');
-});
-
-Route::get('/login/dashboard/kategori_buku', function () {
-    return view('kategoribuku.kategori_buku');
-});
-Route::get('/login/dashboard/peminjam', function () {
-    return view('peminjam.Peminjam');
-});   
-Route::get('/login/dashboard/user', function () {
-    return view('User.user');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});   
-
